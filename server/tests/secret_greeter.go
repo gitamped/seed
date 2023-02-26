050b9962b39e8b36e3c590e3165b292f5bb33d23
gitamped <126345532+gitamped@users.noreply.github.com>
2023-02-26 13:38:32 -0500

    adds secret greet tests

---
+++ server/tests/secret_greeter.go
@@ -0,0 +1,115 @@
+package tests
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/gitamped/seed/auth"
+	"github.com/gitamped/seed/server"
+	"github.com/gitamped/seed/validate"
+)
+
+// GreeterService is a polite API for greeting people.
+type GreeterService interface {
+	// Greet prepares a lovely greeting.
+	Greet(GreetRequest, server.GenericRequest) GreetResponse
+	// SecretGreet requires user to authenticated and authorized before sending lovely greeting.
+	SecretGreet(SecretGreetRequest, server.GenericRequest) SecretGreetResponse
+}
+
+// Required to register endpoints with the Server
+type GreeterRpcService interface {
+	GreeterService
+	// Registers RPCService with Server
+	Register(s *server.Server)
+}
+
+// Implements interface
+type GreeterServicer struct{}
+
+// GreetHandler validates input data prior to calling Greet
+func (gs GreeterServicer) GreetHandler(g server.GenericRequest, b []byte) (any, error) {
+	var gr GreetRequest
+	if err := json.Unmarshal(b, &gr); err != nil {
+		return nil, fmt.Errorf("Unmarshalling data: %w", err)
+	}
+
+	if err := validate.Check(gr); err != nil {
+		return nil, fmt.Errorf("validating data: %w", err)
+	}
+
+	return gs.Greet(gr, g), nil
+}
+
+// Greet implements GreeterRpcService
+func (GreeterServicer) Greet(req GreetRequest, gr server.GenericRequest) GreetResponse {
+	return GreetResponse{
+		Greeting: fmt.Sprintf("Hello %s, the current time is %s", req.Name, gr.Values.Now),
+	}
+}
+
+// SecretGreetHandler validates input data prior to calling SecretGreet
+func (gs GreeterServicer) SecretGreetHandler(g server.GenericRequest, b []byte) (any, error) {
+	var gr SecretGreetRequest
+	if err := json.Unmarshal(b, &gr); err != nil {
+		return nil, fmt.Errorf("Unmarshalling data: %w", err)
+	}
+
+	if err := validate.Check(gr); err != nil {
+		return nil, fmt.Errorf("validating data: %w", err)
+	}
+
+	return gs.SecretGreet(gr, g), nil
+}
+
+// SecretGreet implements GreeterRpcService
+func (GreeterServicer) SecretGreet(req SecretGreetRequest, gr server.GenericRequest) SecretGreetResponse {
+	return SecretGreetResponse{
+		SecretGreeting: fmt.Sprintf("Hello %s, meet at %s", req.Alias, gr.Values.Now.Add(time.Hour*2)),
+	}
+}
+
+// Register implements GreeterRpcService
+func (gs GreeterServicer) Register(s *server.Server) {
+	s.Register("GreeterService", "Greet", server.RPCEndpoint{Roles: []string{}, Handler: gs.GreetHandler})
+	s.Register("GreeterService", "SecretGreet", server.RPCEndpoint{Roles: []string{auth.RoleUser}, Handler: gs.SecretGreetHandler})
+
+}
+
+// Create new GreeterServicer
+func NewGreeterServicer() GreeterRpcService {
+	return GreeterServicer{}
+}
+
+// GreetRequest is the request object for GreeterService.Greet.
+type GreetRequest struct {
+	// Name is the person to greet.
+	// It is required.
+	Name string
+}
+
+// GreetResponse is the response object containing a
+// person's greeting.
+type GreetResponse struct {
+	// Greeting is a nice message welcoming somebody.
+	Greeting string
+	// Error message if request was not successful
+	Error string
+}
+
+// SecretGreetRequest is the request object for GreeterService.SecretGreet.
+type SecretGreetRequest struct {
+	// Alias is the person to greet.
+	// It is required.
+	Alias string
+}
+
+// SecretGreetResponse is the response object containing a
+// person's greeting.
+type SecretGreetResponse struct {
+	// SecretGreeting is a secret message.
+	SecretGreeting string
+	// Error message if request was not successful
+	Error string
+}
---
+++ server/tests/server_test.go
@@ -0,0 +1,162 @@
+package tests
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/gitamped/seed/auth"
+	"github.com/gitamped/seed/keystore"
+	"github.com/gitamped/seed/mid"
+	"github.com/gitamped/seed/server"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+// Success and failure markers.
+const (
+	Success = "\u2713"
+	Failed  = "\u2717"
+)
+
+type ServerTest struct {
+	app          http.Handler
+	adminToken   string
+	userToken    string
+	invalidToken string
+}
+
+func Test_ServerWithRegisteredEndpoints(t *testing.T) {
+
+	const keyID = "54bb2165-71e1-41a6-af3e-7da4a0e1e2c1"
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+
+	ks := keystore.New()
+	ks.Add(privateKey, keyID)
+	a, err := auth.New(keyID, ks)
+	authMid := mid.AuthMiddleware(a)
+
+	claims := auth.Claims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "seed project",
+			Subject:   "5cf37266-3473-4006-984f-9325122678b7",
+			ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		},
+		Roles: []string{auth.RoleUser},
+	}
+
+	token, err := a.GenerateToken(claims)
+	if err != nil {
+		t.Fatalf("failed to generate token")
+	}
+
+	const keyID2 = "54bb2165-71e1-41a6-af3e-7da4a0e1e2c1"
+	privateKey2, _ := rsa.GenerateKey(rand.Reader, 2048)
+
+	ks2 := keystore.New()
+	ks2.Add(privateKey2, keyID2)
+	a2, _ := auth.New(keyID2, ks2)
+
+	invalidToken, _ := a2.GenerateToken(claims)
+	if err != nil {
+		t.Fatalf("failed to generate token")
+	}
+
+	mw := append([]mid.Middleware{authMid}, mid.CommonMiddleware...)
+	s := server.NewServer(mw)
+
+	mid.MultipleMiddleware(s.DefaultHandler, mid.CommonMiddleware...)
+
+	// Register GreeterServicer
+	gs := NewGreeterServicer()
+	gs.Register(s)
+
+	tests := ServerTest{
+		app:          s,
+		userToken:    token,
+		invalidToken: invalidToken,
+	}
+
+	t.Run("Server", tests.Test_Server)
+}
+
+func (st *ServerTest) Test_Server(t *testing.T) {
+	t.Log("Given the need to call the rpc over http server endpoints")
+	{
+
+		ttable := []struct {
+			TestTitle          string
+			ExpectedStatusCode int
+			ExpectedError      bool
+			ExpectedErrorText  string
+			Token              string
+			URL                string
+			RequestData        string
+		}{
+			// test valid name
+			{
+				TestTitle:          "When passed a valid name",
+				ExpectedStatusCode: 200,
+				ExpectedError:      false,
+				Token:              st.userToken,
+				URL:                "/v1/GreeterService.SecretGreet",
+				RequestData:        `{"alias": "Seed Client"}`,
+			},
+			// test invalid user
+			{
+				TestTitle:          "When unauthenticated user attempts action",
+				ExpectedStatusCode: 401,
+				ExpectedError:      true,
+				ExpectedErrorText:  "attempted action is not allowed",
+				Token:              st.invalidToken,
+				URL:                "/v1/GreeterService.SecretGreet",
+				RequestData:        `{"name": "Suspicious Seed Client"}`,
+			},
+		}
+
+		for i, td := range ttable {
+
+			testID := i
+			t.Logf("\tTest %d:\t%s", testID, td.TestTitle)
+			{
+
+				r := httptest.NewRequest(http.MethodPost, td.URL, bytes.NewBuffer([]byte(td.RequestData)))
+
+				w := httptest.NewRecorder()
+				r.Header.Set("Authorization", "Bearer "+td.Token)
+				st.app.ServeHTTP(w, r)
+
+				if w.Code != td.ExpectedStatusCode {
+					t.Fatalf("\t%s\tTest %d:\tShould receive a status code of %d for the response : %v", Failed, testID, td.ExpectedStatusCode, w.Code)
+				}
+				t.Logf("\t%s\tTest %d:\tShould receive a status code of %d for the response.", Success, testID, td.ExpectedStatusCode)
+
+				if w.Code != 200 {
+					continue
+				}
+
+				var got SecretGreetResponse
+				if err := json.NewDecoder(w.Body).Decode(&got); err != nil {
+					t.Fatalf("\t%s\tTest %d:\tShould be able to unmarshal the response : %v", Failed, testID, err)
+				}
+
+				if td.ExpectedError {
+					if got.Error != td.ExpectedErrorText {
+						t.Fatalf("\t%s\tTest %d: Should return the error expected: %s: actual: %s", Failed, testID, td.ExpectedErrorText, got.Error)
+					}
+					t.Logf("\t%s\tTest: Should return the error.", Success)
+				} else {
+					if len(got.SecretGreeting) < 1 {
+						t.Fatalf("\t%s\tTest %d: Should return the expected greeting.", Failed, testID)
+					}
+					t.Logf("\t%s\tTest %d: Should return the expected greeting.", Success, testID)
+				}
+			}
+		}
+	}
+
+}
